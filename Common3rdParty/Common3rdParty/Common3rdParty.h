294ac4c8f1add1a905878ad6b10de13b097e1a6a
sudo.park <sudo.park@kakao.com>
2023-03-19 15:13:59 +0900

    [#3] 초기 프로젝트 세팅

---
+++ Common3rdParty/Common3rdParty/Common3rdParty.h
@@ -0,0 +1,18 @@
+//
+//  Common3rdParty.h
+//  Common3rdParty
+//
+//  Created by sudo.park on 2023/03/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Common3rdParty.
+FOUNDATION_EXPORT double Common3rdPartyVersionNumber;
+
+//! Project version string for Common3rdParty.
+FOUNDATION_EXPORT const unsigned char Common3rdPartyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common3rdParty/PublicHeader.h>
+
+
---
+++ UnitTestHelpKit/UnitTestHelpKit/UnitTestHelpKit.h
@@ -0,0 +1,18 @@
+//
+//  UnitTestHelpKit.h
+//  UnitTestHelpKit
+//
+//  Created by sudo.park on 2023/03/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnitTestHelpKit.
+FOUNDATION_EXPORT double UnitTestHelpKitVersionNumber;
+
+//! Project version string for UnitTestHelpKit.
+FOUNDATION_EXPORT const unsigned char UnitTestHelpKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitTestHelpKit/PublicHeader.h>
+
+
