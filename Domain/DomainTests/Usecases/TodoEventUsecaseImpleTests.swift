8e1ea7966c7262a06f9feef1e295cc761e612e0c
sudo.park <sudo.park@kakao.com>
2023-03-29 00:37:59 +0900

    [#4] TodoEventUsecase 생성 및 수정 로직 구현

--- Domain/DomainTests/Usecases/TodoEventUsecaseImpleTests.swift
+++ Domain/DomainTests/Usecases/TodoEventUsecaseImpleTests.swift
@@ -50,7 +50,7 @@ extension TodoEventUsecaseImpleTests {
         let usecase = self.makeUsecase()
         // when
         let  params = TodoMakeParams()
-            |> \.name .~ "new"
+        |> \.name .~ "new"
         let newTodo = try? await usecase.makeTodoEvent(params)
         
         // then
@@ -64,7 +64,7 @@ extension TodoEventUsecaseImpleTests {
         
         // when
         let params = TodoMakeParams()
-            |> \.eventTagId .~ "some"
+        |> \.eventTagId .~ "some"
         let newTodo = try? await usecase.makeTodoEvent(params)
         
         // then
@@ -73,7 +73,7 @@ extension TodoEventUsecaseImpleTests {
     
     // 생성되면 sharedDataStore에 todoEvent 새로 생성된 이벤트 추가됨
     // -> 추후에는 current todo나, 특정기간동안의 todo가 업데이트 되는지 수정 필요
-    func testUcase_whenTodoMade_shared() {
+    func testUsecase_whenTodoMade_shared() {
         // given
         let expect = expectation(description: "생성되면 sharedDataStore에 todoEvent 새로 생성된 이벤트 추가됨")
         expect.expectedFulfillmentCount = 2
@@ -98,8 +98,55 @@ extension TodoEventUsecaseImpleTests {
     }
     
     // 수정 이후에 수정된 값 반환
+    func testUsecase_updateTodoEvent() async {
+        // given
+        let usecase = self.makeUsecase()
+        
+        // when
+        let params = TodoEditParams()
+            |> \.eventTagId .~ "some"
+        let updated = try? await usecase.updateTodoEvent("id", params)
+        
+        // then
+        XCTAssertNotNil(updated)
+    }
     
     // 수정시 필요한 정보 없으면 실패
+    func testUsecase_whenUpdateTodoEventWithInvalidParams_fail() async {
+        // given
+        let usecase = self.makeUsecase()
+        
+        // when
+        let params = TodoEditParams()
+        let updated = try? await usecase.updateTodoEvent("id", params)
+        
+        // then
+        XCTAssertNil(updated)
+    }
     
     // 수정 이후에 sharedDataStore에 저장된값 업데이트
+    func testUsecase_whenUpdateTodoEvent_updateSharedTodoEvent() {
+        // given
+        let expect = expectation(description: "todo 이벤트 업데이트시에 공유되는 정보에 업데이트된 값 전달")
+        expect.expectedFulfillmentCount = 2
+        let usecase = self.makeUsecase()
+        let shareKey = ShareDataKeys.todos.rawValue
+        self.spyStore.put([String: TodoEvent].self, key: shareKey, [
+            "id": TodoEvent(uuid: "id", name: "old")
+        ])
+        
+        // when
+        let todoSource = self.spyStore.observe([String: TodoEvent].self, key: shareKey)
+        let todos = self.waitOutputs(expect, for: todoSource) {
+            Task {
+                let params = TodoEditParams()
+                    |> \.name .~ "new"
+                _ = try? await usecase.updateTodoEvent("id", params)
+            }
+        }
+        
+        // then
+        let todoNames = todos.map { $0?["id"]?.name }
+        XCTAssertEqual(todoNames, ["old", "new"])
+    }
 }
