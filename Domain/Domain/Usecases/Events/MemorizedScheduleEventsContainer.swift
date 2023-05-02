7369623fea4d782ba99270e1f1bd41a0dcdc35d9
sudo.park <sudo.park@kakao.com>
2023-05-03 03:27:42 +0900

    [#17] MemorizedScheduleEventsContainer 기본 구현
    - 외부로 저장된 ScheduleEvent 제공시, 조회된 기간 내 반복 이벤트들은 계산되어있어야함
    - 혹은 명시적으로 refresh 인터페이스를 이용하여 주어진 이벤트들의 반복시간들을 계산할 수 있음
    - 일정 반복시간 계산시에는 캐싱된 값을 활용, 세부 절차는 아래와 같음
    ㄴ 주어진 범위와 겹치는 캐시가 없다면 새로 계산
    ㄴ 주어진 범위를 포함하는 캐시가 있다면 -> 캐시 리턴
    ㄴ 주어진 범위의 시작시간을 포함하는 캐시가 있다면 캐시 범위를 캐시..<end로 확장하고 캐시마지막..<end 까지의 범위를 재귀적으로 계산해서 합산
    ㄴ 주어진 범위의 시작시간을 포함하는 캐시가 없으면, 시작시간 이후 종료시간 전까지 존재하는 캐시를 검사 -> 캐시 범위를 start..<max(block.end, end) 로 확장하고 block너머의 영역은 필요시 재귀적으로 계산해서 합산

--- Domain/Domain/Usecases/Events/MemorizedScheduleEventsContainer.swift
+++ Domain/Domain/Usecases/Events/MemorizedScheduleEventsContainer.swift
@@ -12,52 +12,71 @@ import Optics
 
 struct MemorizedScheduleEventsContainer {
         
-    private struct Item {
+    private struct CacheItem {
         
         var event: ScheduleEvent
-        var calcualtedRanges: [Range<TimeStamp>] = []
+        var calculatedRangeAndTimes: [(Range<TimeStamp>, [ScheduleEvent.RepeatingTimes])] = []
         
         init(event: ScheduleEvent) {
             self.event = event
         }
         
         func isNotCalculated(for period: Range<TimeStamp>) -> Bool {
-            return self.calcualtedRanges.first(where: { period.isSubRange(of: $0) }) == nil
+            return self.event.repeating != nil
+                && self.calculatedRangeAndTimes
+                .first(where: { period.isSubRange(of: $0.0) }) == nil
+        }
+        
+        func firstCacheIndex(contains time: TimeStamp) -> Int? {
+            return self.calculatedRangeAndTimes.firstIndex(where: {
+                $0.0.contains(time)
+            })
+        }
+        
+        func nextCacheIndex(from time: TimeStamp, until end: TimeStamp) -> Int? {
+            return self.calculatedRangeAndTimes.firstIndex(where: {
+                return time < $0.0.lowerBound && $0.0.lowerBound < end
+            })
+        }
+        
+        mutating func append(new : (Range<TimeStamp>, [ScheduleEvent.RepeatingTimes])) {
+            self.calculatedRangeAndTimes.append(new)
+            self.event.repeatingTimes = self.calculatedRangeAndTimes.flatMap { $0.1 }
         }
     }
     
-    private var items: [String: Item] = [:]
+    private var caches: [String: CacheItem] = [:]
 }
 
 
 extension MemorizedScheduleEventsContainer {
     
     func scheduleEvents(in period: Range<TimeStamp>) -> [ScheduleEvent] {
-        return self.items.values
+        return self.caches.values
             .map { $0 }
             .filter { $0.event.isOverlap(with: period) }
-            .map { item in
-                guard item.isNotCalculated(for: period) else { return item }
-                return self.calculateRepeatingTimes(item.event, with: item, in: period)
+            .map { cache in
+                guard cache.isNotCalculated(for: period) else { return cache }
+                return self.calculateRepeatingTimes(cache.event, with: cache, in: period)
             }
             .map { $0.event }
     }
     
     func append(_ newEvent: ScheduleEvent) -> MemorizedScheduleEventsContainer {
         return MemorizedScheduleEventsContainer(
-            items: self.items |> key(newEvent.uuid) .~ .init(event: newEvent)
+            caches: self.caches |> key(newEvent.uuid) .~ .init(event: newEvent)
         )
     }
     
     func refresh(_ events: [ScheduleEvent], in period: Range<TimeStamp>) -> MemorizedScheduleEventsContainer {
         
-        let cachedInPeriodMap = self.items.values
+        let cachedInPeriodMap = self.caches.values
             .filter { $0.event.isOverlap(with: period) }
             .asDictionary { $0.event.uuid }
         let newEventsMap = events.asDictionary { $0.uuid }
         
         let removed = cachedInPeriodMap.filter { newEventsMap[$0.key] == nil }
-        var newItems = self.items.filter { removed[$0.value.event.uuid] != nil }
+        var newItems = self.caches.filter { removed[$0.value.event.uuid] != nil }
         
         let newEventsWithCalculate = events.map {
             self.calculateRepeatingTimes($0, with: cachedInPeriodMap[$0.uuid], in: period)
@@ -66,21 +85,143 @@ extension MemorizedScheduleEventsContainer {
             newItems[$0.event.uuid] = $0
         }
         
-        return .init(items: newItems)
+        return .init(caches: newItems)
     }
     
     private func calculateRepeatingTimes(
         _ event: ScheduleEvent,
-        with cached: Item?,
+        with cached: CacheItem?,
         in period: Range<TimeStamp>
-    ) -> Item {
+    ) -> CacheItem {
         guard let repeating = event.repeating
         else {
             return .init(event: event)
         }
+        let cached = cached ?? CacheItem(event: event)
+        guard let enumerator = EventRepeatTimeEnumerator(repeating.repeatOption)
+        else { return cached }
         
-        // TODO: calculate with cache
-        return .init(event: event)
+        let (startTime, end) = (
+            event.time.shift(to: repeating.repeatingStartTime),
+            repeating.repeatingEndTime.map { min($0, period.upperBound) } ?? period.upperBound
+        )
+        
+        let calculatedResult = self.calculateRepeatingTimesBlock(
+            enumerator,
+            from: .init(time: startTime, turn: 1),
+            unitl: end,
+            acc: .empty(cached)
+        )
+        guard let newRange = calculatedResult.newRange
+        else {
+            return cached
+        }
+        var newItem = calculatedResult.cacheItem
+        newItem.append(new: (newRange, calculatedResult.newCalculated))
+        
+        return newItem
+    }
+    
+    private struct BlockCalculateResult {
+        let newRange: Range<TimeStamp>?
+        let newCalculated: [ScheduleEvent.RepeatingTimes]
+        var cacheItem: CacheItem
+        init(
+            _ newRange: Range<TimeStamp>?,
+            _ newCalculated: [ScheduleEvent.RepeatingTimes],
+            _ cacheItem: CacheItem
+        ) {
+            self.newRange = newRange
+            self.newCalculated = newCalculated
+            self.cacheItem = cacheItem
+        }
+        
+        static func empty(_ cachedItem: CacheItem) -> BlockCalculateResult {
+            return .init(nil, [], cachedItem)
+        }
+    }
+    
+    // 캐싱된 값이 많아질수록(파편이 많을수록) 연산량이 늘어나고 재귀걸리다 스택 터질수도있음
+    // 파편화된 캐시 블럭을 합쳐주는 로직이 잘 돌아가야함
+    // 대부분 조회는 1달 단위로 조회하고 또한 캐싱도 한달 단위로 된다고 했을때 대부분 return 2에 걸릴것으로 예상
+    // 달 조회를 많이 할수록 파편화돤 캐시블럭은 많아지겠지만 계산시에 return 2 or return 5에 걸리고 해당 연산량은 그리 크지 않을것으로 예상
+    private func calculateRepeatingTimesBlock(
+        _ enumerator: EventRepeatTimeEnumerator,
+        from start: ScheduleEvent.RepeatingTimes,
+        unitl end: TimeStamp,
+        acc result: BlockCalculateResult
+    ) -> BlockCalculateResult {
+        let startTimeStamp = start.time.lowerBoundTimeStamp
+        // return 1
+        guard startTimeStamp < end else { return result }
+        
+        var result = result
+        
+        if let blockIndex = result.cacheItem.firstCacheIndex(contains: startTimeStamp) {
+            // 현재 시작점을 포함하는 범위의 캐시가 존재한다면
+            // cache.start..<start..<(cache.end or end)
+            let (blockRange, block) = result.cacheItem.calculatedRangeAndTimes.remove(at: blockIndex)
+            guard end > blockRange.upperBound
+            else{
+                // return2: block.start..period..<block.end -> period를 포함하는 캐시를 리턴
+                return .init(blockRange, block, result.cacheItem)
+            }
+            
+            // block.start..period.start..block.end..<period.end
+            // -> subBlock1: cache -> block.start..<block.end
+            // -> subBlock2: block.end..<period.end
+            let block_to_end = self.calculateRepeatingTimesBlock(
+                enumerator, from: block.last ?? start, unitl: end, acc: result
+            )
+            // return 3
+            return .init(
+                blockRange.lowerBound..<end,
+                block + block_to_end.newCalculated,
+                block_to_end.cacheItem
+            )
+            
+        } else if let blockIndex = result.cacheItem.nextCacheIndex(from: startTimeStamp, until: end) {
+            // start..<cache.start..<end 내에 캐시가 존재한다면
+            let (blockRange, block) = result.cacheItem.calculatedRangeAndTimes.remove(at: blockIndex)
+            
+            // leftBlock: start..<block.start
+            // centerBlock: cache -> block.start..<block.end
+            // rightBlock: block.end..<end
+            
+            let newEnd = max(end, blockRange.upperBound)
+            let start_to_block = self.enumerateEventTimes(
+                enumerator, from: start, until: blockRange.lowerBound
+            )
+            let block_to_end = self.calculateRepeatingTimesBlock(
+                enumerator, from: block.last ?? start, unitl: newEnd, acc: result
+            )
+            // return 4
+            return .init(
+                startTimeStamp..<newEnd,
+                start_to_block + block + block_to_end.newCalculated,
+                block_to_end.cacheItem
+            )
+        } else {
+            // return 5 캐싱된것이 없다면 주어진 범위까지 계산
+            return .init(
+                startTimeStamp..<end,
+                self.enumerateEventTimes(enumerator, from: start, until: end),
+                result.cacheItem
+            )
+        }
+    }
+    
+    private func enumerateEventTimes(
+        _ enumerator: EventRepeatTimeEnumerator,
+        from start: ScheduleEvent.RepeatingTimes,
+        until end: TimeStamp
+    ) -> [ScheduleEvent.RepeatingTimes] {
+        let nextFirstTurn = start.turn
+        return enumerator.nextEventTimes(from: start.time, until: end)
+            .enumerated()
+            .map { pair -> ScheduleEvent.RepeatingTimes in
+                return .init(time: pair.element, turn: nextFirstTurn + pair.offset)
+            }
     }
 }
 
@@ -90,4 +231,12 @@ private extension Range where Bound == TimeStamp {
         return parentRange.lowerBound <= self.lowerBound
             && self.upperBound <= parentRange.upperBound
     }
+}
+
+private func min(_ lhs: TimeStamp, _ rhs: TimeStamp) -> TimeStamp {
+    return lhs.utcTimeInterval <= rhs.utcTimeInterval ? lhs : rhs
+}
+
+private func max(_ lhs: TimeStamp, _ rhs: TimeStamp) -> TimeStamp {
+    return lhs.utcTimeInterval >= rhs.utcTimeInterval ? lhs : rhs
 }
