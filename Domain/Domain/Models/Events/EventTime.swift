e37c27654dcc3d589ba119b7f68d7f67641628bc
sudo.park <sudo.park@kakao.com>
2023-05-03 03:27:42 +0900

    [#17] MemorizedScheduleEventsContainer 기본 구현
    - 외부로 저장된 ScheduleEvent 제공시, 조회된 기간 내 반복 이벤트들은 계산되어있어야함
    - 혹은 명시적으로 refresh 인터페이스를 이용하여 주어진 이벤트들의 반복시간들을 계산할 수 있음
    - 일정 반복시간 계산시에는 캐싱된 값을 활용, 세부 절차는 아래와 같음
    ㄴ 주어진 범위와 겹치는 캐시가 없다면 새로 계산
    ㄴ 주어진 범위를 포함하는 캐시가 있다면 -> 캐시 리턴
    ㄴ 주어진 범위의 시작시간을 포함하는 캐시가 있다면 캐시 범위를 캐시..<end로 확장하고 캐시마지막..<end 까지의 범위를 재귀적으로 계산해서 합산
    ㄴ 주어진 범위의 시작시간을 포함하는 캐시가 없으면, 시작시간 이후 종료시간 전까지 존재하는 캐시를 검사 -> 캐시 범위를 start..<max(block.end, end) 로 확장하고 block너머의 영역은 필요시 재귀적으로 계산해서 합산

--- Domain/Domain/Models/Events/EventTime.swift
+++ Domain/Domain/Models/Events/EventTime.swift
@@ -14,11 +14,18 @@ public enum EventTime: Comparable {
     
     case at(TimeStamp)
     case period(Range<TimeStamp>)
-    
+
     var timeZoneAbbreviation: String {
         switch self {
         case .at(let time): return time.timeZoneAbbreviation
         case .period(let range): return range.lowerBound.timeZoneAbbreviation
+        }
+    }
+    
+    var lowerBoundTimeStamp: TimeStamp {
+        switch self {
+        case .at(let time): return time
+        case .period(let range): return range.lowerBound
         }
     }
     
@@ -51,6 +58,17 @@ public enum EventTime: Comparable {
             return .at(time.add(interval))
         case .period(let range):
             return .period(range.lowerBound.add(interval)..<range.upperBound.add(interval))
+        }
+    }
+    
+    func shift(to timeStamp: TimeStamp) -> EventTime {
+        switch self {
+        case .at(let time):
+            let interval = timeStamp.utcTimeInterval - time.utcTimeInterval
+            return .at(time.add(interval))
+        case .period(let ranege):
+            let interval = timeStamp.utcTimeInterval - ranege.lowerBound.utcTimeInterval
+            return .period(ranege.lowerBound.add(interval)..<ranege.upperBound.add(interval))
         }
     }
     
