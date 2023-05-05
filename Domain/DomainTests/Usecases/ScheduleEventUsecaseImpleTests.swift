26db3cdef633fa23b9ed26d05c20c071b4d944b9
sudo.park <sudo.park@kakao.com>
2023-05-05 17:52:11 +0900

    [#17] ScheduleEventUsecaseImple 조회시에 반복일정 반복시간 계산여부 tc 추가

--- Domain/DomainTests/Usecases/ScheduleEventUsecaseImpleTests.swift
+++ Domain/DomainTests/Usecases/ScheduleEventUsecaseImpleTests.swift
@@ -47,6 +47,25 @@ final class ScheduleEventUsecaseImpleTests: BaseTestCase, PublisherWaitable {
     private func stubMakeFail() {
         self.stubRepository.shouldFailMake = true
     }
+    
+    private func notReapeatingEvent(at day: Int) -> ScheduleEvent {
+        let time = EventTime.at(.dummy(0).add(TimeInterval(day) * 24 * 3600))
+        return .init(uuid: "id:\(day)", name: "name", time: time)
+    }
+    
+    private func repeatingEvent(at day: Int) -> ScheduleEvent {
+        let time = EventTime.at(.dummy(0).add(TimeInterval(day) * 24 * 3600))
+        let repeating = EventRepeating(
+            repeatingStartTime: time.lowerBoundTimeStamp,
+            repeatOption: EventRepeatingOptions.EveryDay()
+        )
+        return .init(uuid: "id:\(day)", name: "name", time: time)
+            |> \.repeating .~ repeating
+    }
+    
+    private func stubEvents(_ events: [ScheduleEvent]) {
+        self.stubRepository.eventsMocking = { _ in events }
+    }
 }
 
 // MARK: - make case
@@ -124,6 +143,13 @@ extension ScheduleEventUsecaseImpleTests {
         ) { _ in .init() }
     }
     
+    private func appendMemorized(_ event: ScheduleEvent) {
+        self.spyStore.update(
+            MemorizedScheduleEventsContainer.self,
+            key: ShareDataKeys.schedules.rawValue
+        ) { ($0 ?? .init()).append(event) }
+    }
+    
     func testUsecase_scheduleEventsInPeriod_withMemorized() {
         // given
         let expect = expectation(description: "캐시 있는 경우 range에 해당하는 이벤트 조회 및 refresh")
@@ -191,7 +217,82 @@ extension ScheduleEventUsecaseImpleTests {
         ])
     }
     
-    // range에 해당하는 이벤트 조회시에 해당 기간 내 이벤트 반복시간들도 계산되어있어야함
+    func testUsecase_whenObserveEventsInRangeAndEventIsRepeating_shouldBeCalculatedEventRepeaintTime() {
+        // given
+        let expect = expectation(description: "range에 해당하는 이벤트 조회시에 해당 기간 내 이벤트 반복시간들도 계산되어있어야함")
+        let usecase = self.makeUsecase()
+        self.stubNoMemorized()
+        self.appendMemorized(self.repeatingEvent(at: 100))
+        self.stubEvents([
+            self.notReapeatingEvent(at: 13),
+            self.repeatingEvent(at: 10),
+        ])
+        
+        // when
+        let source = usecase.scheduleEvents(in: self.dummyRange).filter { $0.isEmpty == false }
+        let events = self.waitFirstOutput(expect, for: source) {
+            usecase.refreshScheduleEvents(in: self.dummyRange)
+        } ?? []
+        
+        // then
+        XCTAssertEqual(events.count, 2)
+        let notRepeatingEvent = events.first(where: { $0.uuid == "id:13" })
+        let repeatingEvent = events.first(where: { $0.uuid == "id:10" })
+        XCTAssertEqual(notRepeatingEvent?.repeatingTimes.count, 1)
+        XCTAssertEqual(repeatingEvent?.repeatingTimes.count, 11)
+    }
     
-    // 새로 생성되어 append된 이벤트도 range로 조회시에 기간에 포함되면 반복시간 계산해서 반환해야함
+    func testUsecase_whenMakeNewRepeatingEventDuringObserving_update() {
+        // given
+        let expect = expectation(description: "새로 생성되어 append된 이벤트도 range로 조회시에 기간에 포함되면 반복시간 계산해서 반환해야함")
+        let usecas = self.makeUsecase()
+        self.stubNoMemorized()
+        
+        // when
+        let source = usecas.scheduleEvents(in: self.dummyRange).filter { $0.isEmpty == false }
+        let events = self.waitFirstOutput(expect, for: source) {
+            
+            Task.init {
+                let params = ScheduleMakeParams()
+                    |> \.name .~ "new"
+                    |> \.time .~ .at(.dummy(0).add(3))
+                |> \.repeating .~ .init(
+                    repeatingStartTime: .dummy(0).add(3),
+                    repeatOption: EventRepeatingOptions.EveryDay()
+                )
+                _ = try? await usecas.makeScheduleEvent(params)
+            }
+        }
+        
+        // then
+        let event = events?.first(where: { $0.uuid == "new" })
+        XCTAssertEqual(event?.repeatingTimes.count, 20)
+    }
+    
+    func testUsecase_whenMakeNewRepeatingEventDuringObservingButRangeNotOverlap_notUpdate() {
+        // given
+        let expect = expectation(description: "새로 생성되어 append된 이벤트도 range로 조회시에 기간에 포함 안되면 이벤트 발생 안함")
+        expect.isInverted = true
+        let usecas = self.makeUsecase()
+        self.stubNoMemorized()
+        
+        // when
+        let source = usecas.scheduleEvents(in: self.dummyRange).filter { $0.isEmpty == false }
+        let events = self.waitFirstOutput(expect, for: source) {
+            
+            Task.init {
+                let paramsRangeOver = ScheduleMakeParams()
+                    |> \.name .~ "over"
+                    |> \.time .~ .at(.dummy(0).add(24 * 3600 * 100))
+                    |> \.repeating .~ .init(
+                        repeatingStartTime: .dummy(0).add(24 * 3600 * 100),
+                        repeatOption: EventRepeatingOptions.EveryDay()
+                    )
+                _ = try? await usecas.makeScheduleEvent(paramsRangeOver)
+            }
+        }
+        
+        // then
+        XCTAssertNil(events)
+    }
 }
