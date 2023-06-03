3666a2b3817060f2d1868d1c3845ffbf727b8a20
sudo.park <sudo.park@kakao.com>
2023-06-04 08:41:52 +0900

    [#33] CommonPresenting 및 CalendarScenes framework 추가

---
+++ CommonPresentation/CommonPresentation/CommonPresentation.h
@@ -0,0 +1,18 @@
+//
+//  CommonPresentation.h
+//  CommonPresentation
+//
+//  Created by sudo.park on 2023/06/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonPresentation.
+FOUNDATION_EXPORT double CommonPresentationVersionNumber;
+
+//! Project version string for CommonPresentation.
+FOUNDATION_EXPORT const unsigned char CommonPresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonPresentation/PublicHeader.h>
+
+
