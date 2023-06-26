90d6d4f76e2191410526457d9df640eebb1bb108
sudo.park <sudo.park@kakao.com>
2023-06-26 23:34:54 +0900

    [#40] TestDoubles 프레임워크 추가 및 StubHolidayUsecase, StubCalendarSettingUsecase 추가

---
+++ TestDoubles/TestDoubles/TestDoubles.h
@@ -0,0 +1,18 @@
+//
+//  TestDoubles.h
+//  TestDoubles
+//
+//  Created by sudo.park on 2023/06/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestDoubles.
+FOUNDATION_EXPORT double TestDoublesVersionNumber;
+
+//! Project version string for TestDoubles.
+FOUNDATION_EXPORT const unsigned char TestDoublesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestDoubles/PublicHeader.h>
+
+
---
+++ TestDoubles/TestDoubles/Usecases/StubCalendarSettingUsecase.swift
@@ -0,0 +1,48 @@
+//
+//  StubCalendarSettingUsecase.swift
+//  TestDoubles
+//
+//  Created by sudo.park on 2023/06/26.
+//
+
+import Foundation
+import Combine
+import Domain
+
+
+open class StubCalendarSettingUsecase: CalendarSettingUsecase {
+    
+    public init() { }
+    
+    private let firstWeekDaySubject = CurrentValueSubject<DayOfWeeks?, Never>(nil)
+    open func updateFirstWeekDay(_ newValue: DayOfWeeks) {
+        self.firstWeekDaySubject.send(newValue)
+    }
+    
+    open var firstWeekDay: AnyPublisher<DayOfWeeks, Never> {
+        return self.firstWeekDaySubject
+            .compactMap { $0 }
+            .eraseToAnyPublisher()
+    }
+    
+    open func loadAllTimeZones() -> [TimeZone] {
+        return [
+            TimeZone(abbreviation: "KST"),
+            TimeZone(identifier: "Africa/Abidjan"),
+            TimeZone(identifier: "America/Cayman"),
+            TimeZone(identifier: "Pacific/Bougainville")
+        ]
+        .compactMap { $0 }
+    }
+    
+    private let currentTimeZoneSubject = CurrentValueSubject<TimeZone?, Never>(nil)
+    open func selectTimeZone(_ timeZone: TimeZone) {
+        self.currentTimeZoneSubject.send(timeZone)
+    }
+    
+    open var currentTimeZone: AnyPublisher<TimeZone, Never> {
+        return self.currentTimeZoneSubject
+            .compactMap { $0 }
+            .eraseToAnyPublisher()
+    }
+}
