139433f54ee1f7e9317c8632126ecfc3fa2976e1
sudo.park <sudo.park@kakao.com>
2023-08-05 14:10:46 +0900

    [#66] ViewAppearance 추가
    - 해당 객체는 observableObject로 관리
    - colorSet, fontSet 이 변경될때 외부로 이벤트 전파 및 업데이트 예정
    - 기본 light colorSet, systemFont set 추가

---
+++ CommonPresentation/CommonPresentation/Appearance/FontSet.swift
@@ -0,0 +1,34 @@
+//
+//  FontSet.swift
+//  CommonPresentation
+//
+//  Created by sudo.park on 2023/08/05.
+//
+
+import UIKit
+
+
+// MARK: - FontSet
+
+public protocol FontSet: Sendable {
+    
+    // calendar component
+    var weekday: UIFont { get }
+    var day: UIFont { get }
+    var eventOnDay: UIFont { get }
+}
+
+
+public enum FontSetKeys: String {
+    case systemDefault
+}
+
+// MARK: - default font set
+
+public struct SystemDefaultFontSet: FontSet {
+
+    // calendar component
+    public let weekday: UIFont = UIFont.systemFont(ofSize: 12)
+    public let day: UIFont = UIFont.systemFont(ofSize: 14)
+    public let eventOnDay: UIFont = UIFont.systemFont(ofSize: 10)
+}
