fe6df3fb99dcb58221bc7a018b6db1f48c96fa37
sudo.park <sudo.park@kakao.com>
2023-08-11 00:30:29 +0900

    [#44] Pubisher async extension 추가
    - await로 value를 읽는 경우 timeout 추가

---
+++ UnitTestHelpKit/UnitTestHelpKit/Publisher+async+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Publisher+async+Extensions.swift
+//  UnitTestHelpKit
+//
+//  Created by sudo.park on 2023/08/11.
+//
+
+import Foundation
+import Combine
+
+
+extension Publisher {
+    
+    public func values(with timeoutMillis: Int) async throws -> AsyncThrowingPublisher<AnyPublisher<Output, Failure>> {
+        
+        let sender = self.values
+        return self.timeout(.milliseconds(timeoutMillis), scheduler: RunLoop.main)
+            .eraseToAnyPublisher()
+            .values
+    }
+    
+    public func firstValue(with timeoutMillis: Int) async throws -> Output? {
+        return try await self.values(with: timeoutMillis)
+            .first(where: { _ in true })
+    }
+}
