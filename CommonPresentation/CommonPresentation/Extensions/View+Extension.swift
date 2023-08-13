738e332b4f9a3fea8a7ec9d01147f974782ae5b9
sudo.park <sudo.park@kakao.com>
2023-08-14 01:44:13 +0900

    [#72] eventHandler 추가 및 SIngleMonthView의 interaction은 eventHandler를 통해 hosting vc에서 vm으로 이벤트 전달하도록 수정

--- CommonPresentation/CommonPresentation/Extensions/View+Extension.swift
+++ CommonPresentation/CommonPresentation/Extensions/View+Extension.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
+import Prelude
+import Optics
 
 extension View {
     
     public func asAnyView() -> AnyView {
         return AnyView(self)
+    }
+    
+    public func eventHandler<Handler>(
+        _ keyPath: WritableKeyPath<Self, Handler>,
+        _ handler: Handler
+    ) -> Self {
+        return self |> keyPath .~ handler
     }
 }
