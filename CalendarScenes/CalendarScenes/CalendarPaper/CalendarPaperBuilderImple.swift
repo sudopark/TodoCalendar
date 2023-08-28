896b142d41ef59d17b689ce5f704bcf81766c551
sudo.park <sudo.park@kakao.com>
2023-08-28 23:22:58 +0900

    [#84] CalendarPaperScene 추가
    - Month, DayEventList Scene을 Hosting하여 child로 붙일 예정

---
+++ CalendarScenes/CalendarScenes/CalendarPaper/CalendarPaperBuilderImple.swift
@@ -0,0 +1,48 @@
+//
+//  
+//  CalendarPaperBuilderImple.swift
+//  CalendarScenes
+//
+//  Created by sudo.park on 2023/08/28.
+//
+//
+
+import UIKit
+import Scenes
+import CommonPresentation
+
+
+// MARK: - CalendarPaperSceneBuilerImple
+
+public final class CalendarPaperSceneBuilerImple {
+    
+    private let viewAppearance: ViewAppearance
+    
+    public init(
+        viewAppearance: ViewAppearance
+    ) {
+        self.viewAppearance = viewAppearance
+    }
+}
+
+
+extension CalendarPaperSceneBuilerImple: CalendarPaperSceneBuiler {
+    
+    public func makeCalendarPaperScene() -> any CalendarPaperScene {
+        
+        let viewModel = CalendarPaperViewModelImple(
+            
+        )
+        
+        let viewController = CalendarPaperViewController(
+            viewModel: viewModel,
+            viewAppearance: self.viewAppearance
+        )
+        
+        let router = CalendarPaperRouter()
+        router.scene = viewController
+        viewModel.router = router
+        
+        return viewController
+    }
+}
---
+++ CalendarScenes/CalendarScenes/CalendarPaper/CalendarPaperRouter.swift
@@ -0,0 +1,31 @@
+//
+//  
+//  CalendarPaperRouter.swift
+//  CalendarScenes
+//
+//  Created by sudo.park on 2023/08/28.
+//
+//
+
+import UIKit
+import Scenes
+import CommonPresentation
+
+
+// MARK: - Routing
+
+protocol CalendarPaperRouting: Routing, Sendable { }
+
+// MARK: - Router
+
+final class CalendarPaperRouter: BaseRouterImple, CalendarPaperRouting, @unchecked Sendable { }
+
+
+extension CalendarPaperRouter {
+    
+    private var currentScene: (any CalendarPaperScene)? {
+        self.scene as? (any CalendarPaperScene)
+    }
+    
+    // TODO: router implememnts
+}
