d001907b011209e8021009417e38f9c16a8fd92f
sudo.park <sudo.park@kakao.com>
2023-10-28 16:57:20 +0900

    [#117] 이벤트 디테일 데이터 저장 및 로드 기능 구현

---
+++ Domain/Domain/Models/Events/EventDetailData.swift
@@ -0,0 +1,45 @@
+//
+//  EventDetailData.swift
+//  Domain
+//
+//  Created by sudo.park on 10/28/23.
+//
+
+import Foundation
+
+
+// MARK: - EventDetail
+
+public struct Place: Sendable {
+
+    public struct Coordinate: Sendable, Equatable {
+        public let latttude: Double
+        public let longitude: Double
+        
+        public init(_ latttude: Double, _ longitude: Double) {
+            self.latttude = latttude
+            self.longitude = longitude
+        }
+    }
+    
+    public let placeName: String
+    public let coordinate: Coordinate
+    public var addressText: String?
+    
+    public init(_ placeName: String, _ coordinate: Coordinate) {
+        self.placeName = placeName
+        self.coordinate = coordinate
+    }
+}
+
+public struct EventDetailData: Sendable {
+    
+    public let eventId: String
+    public var place: Place?
+    public var url: String?
+    public var memo: String?
+    
+    public init(_ eventId: String) {
+        self.eventId = eventId
+    }
+}
---
+++ Repository/Repository/Repository+Imple/Event/EventDetailData/Local/EventDetailDataTable.swift
@@ -0,0 +1,72 @@
+//
+//  EventDetailDataTable.swift
+//  Repository
+//
+//  Created by sudo.park on 10/28/23.
+//
+
+import Foundation
+import SQLiteService
+import Domain
+
+
+struct EventDetailDataTable: Table {
+    
+    enum Columns: String, TableColumn {
+        case uuid
+        case url
+        case memo
+        case placeName = "place_name"
+        case placeAddress = "place_addr"
+        case placeLatitude = "place_lat"
+        case placeLongitude = "place_long"
+        
+        var dataType: ColumnDataType {
+            switch self {
+            case .uuid: return .text([.primaryKey(autoIncrement: false), .unique, .notNull])
+            case .url: return .text([])
+            case .memo: return .text([])
+            case .placeName: return .text([])
+            case .placeAddress: return .text([])
+            case .placeLatitude: return .real([])
+            case .placeLongitude: return .real([])
+            }
+        }
+    }
+    
+    typealias ColumnType = Columns
+    typealias EntityType = EventDetailData
+    static var tableName: String { "EventDetailData" }
+    
+    static func scalar(_ entity: EntityType, for column: Columns) -> (any ScalarType)? {
+        switch column {
+        case .uuid: return entity.eventId
+        case .url: return entity.url
+        case .memo: return entity.memo
+        case .placeName: return entity.place?.placeName
+        case .placeAddress: return entity.place?.addressText
+        case .placeLatitude: return entity.place?.coordinate.latttude
+        case .placeLongitude: return entity.place?.coordinate.longitude
+        }
+    }
+}
+
+extension EventDetailData: RowValueType {
+    
+    public init(_ cursor: CursorIterator) throws {
+        self.init(
+            try cursor.next().unwrap()
+        )
+        self.url = cursor.next()
+        self.memo = cursor.next()
+        
+        let placeName: String? = cursor.next()
+        let placeAddr: String? = cursor.next()
+        let placeLatt: Double? = cursor.next()
+        let placeLong: Double? = cursor.next()
+        guard let name = placeName, let lat = placeLatt, let long = placeLong
+        else { return }
+        self.place = .init(name, .init(lat, long))
+        self.place?.addressText = placeAddr
+    }
+}
