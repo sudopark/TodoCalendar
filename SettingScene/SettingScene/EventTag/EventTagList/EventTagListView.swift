e62d5c18ed9d533cd873f7eb55008bb3e6f8eb09
sudo.park <sudo.park@kakao.com>
2023-10-30 17:38:03 +0900

    [#127] CloseButton 분리

--- SettingScene/SettingScene/EventTag/EventTagList/EventTagListView.swift
+++ SettingScene/SettingScene/EventTag/EventTagList/EventTagListView.swift
@@ -110,18 +110,9 @@ struct EventTagListView: View {
                             )
                             .font(.system(size: 20))
                     }
-                    Button {
-                        self.closeScene()
-                    } label: {
-                        Image(systemName: "xmark.circle.fill")
-                            .symbolRenderingMode(.palette)
-                            .foregroundStyle(
-                                self.appearance.colorSet.event.asColor,
-                                self.appearance.colorSet.eventList.asColor
-                            )
-                            .font(.system(size: 20))
-                            
-                    }
+                    
+                    CloseButton()
+                        .eventHandler(\.onTap, self.closeScene)
                 }
             }
             .navigationTitle("Event Types".localized())
