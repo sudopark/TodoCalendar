b3d37007fba51abad4108b529ca8463c91ca64af
sudo.park <sudo.park@kakao.com>
2023-12-16 14:24:48 +0900

    [#153] EventOnCalendarView 추가

--- SettingScene/SettingScene/Setting/Appearance/AppearanceSettingView.swift
+++ SettingScene/SettingScene/Setting/Appearance/AppearanceSettingView.swift
@@ -14,6 +14,49 @@ import Combine
 import CommonPresentation
 
 
+struct AppearanceRow< Content: View>: View {
+    
+    private let title: String
+    private let subTitle: String?
+    private let content: Content
+    @EnvironmentObject private var appearance: ViewAppearance
+    
+    init(_ title: String, subTitle: String? = nil, _ content: Content) {
+        self.title = title
+        self.subTitle = subTitle
+        self.content = content
+    }
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 2) {
+                Text(title)
+                    .font(self.appearance.fontSet.normal.asFont)
+                    .foregroundStyle(appearance.colorSet.normalText.asColor)
+                
+                if let subTitle {
+                    Text(subTitle)
+                        .font(self.appearance.fontSet.size(10).asFont)
+                        .foregroundStyle(appearance.colorSet.subSubNormalText.asColor)
+                }
+            }
+            
+            Spacer()
+            
+            content
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 10)
+        .background(
+            RoundedRectangle(cornerRadius: 8)
+                .fill(UIColor.systemGroupedBackground.asColor)
+        )
+    }
+}
+
+
+
+
 // MARK: - AppearanceSettingViewState
 
 final class AppearanceSettingViewState: ObservableObject {
