13047d4d792b073e50394dafce39e899789dd9e6
sudo.park <sudo.park@kakao.com>
2024-01-08 23:09:29 +0900

    [#165] 이벤트 알림 관련 모델 추가

---
+++ Domain/Sources/Models/믿ㄱㅅ/EventNotificationTimeOption.swift
@@ -0,0 +1,19 @@
+//
+//  EventNotificationTimeOption.swift
+//  Domain
+//
+//  Created by sudo.park on 1/6/24.
+//  Copyright © 2024 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+
+
+public enum EventNotificationTimeOption {
+    
+    case atTime
+    case before(seconds: TimeInterval)
+    case allDay9AM
+    case allDay12AM
+    case allDay9AMBefore(seconds: TimeInterval)
+}
