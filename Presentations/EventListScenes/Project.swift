4d9e40c7ee4b3d843c38008749b80a8bc5e01892
sudo.park <sudo.park@kakao.com>
2024-05-06 15:49:09 +0900

    [#210] EventListScene 프레임워크 추가

---
+++ Presentations/EventListScenes/Project.swift
@@ -0,0 +1,16 @@
+import ProjectDescriptionHelpers
+import ProjectDescription
+
+let project = Project.frameworkWithTest(
+    name: "EventListScenes",
+    platform: .iOS,
+    iOSTargetVersion: "16.0",
+    dependencies: [
+        .project(target: "Common3rdParty", path: .relativeToCurrentFile("../../Supports/Common3rdParty")),
+        .project(target: "CommonPresentation", path: .relativeToCurrentFile("../../Presentations/CommonPresentation")),
+        .project(target: "Domain", path: .relativeToCurrentFile("../../Domain")),
+        .project(target: "Extensions", path: .relativeToCurrentFile("../../Supports/Extensions")),
+        .project(target: "Scenes", path: .relativeToCurrentFile("../../Presentations/Scenes"))
+    ]
+)
+
