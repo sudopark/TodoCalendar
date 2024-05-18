a33d7b60e2330f14aa5e90590f7e3a4c0fb629c0
sudo.park <sudo.park@kakao.com>
2024-05-18 16:45:47 +0900

    [#212] 위젯타겟 추가

---
+++ TodoCalendarApp/AppExtensions/Widget/Sources/TodoCalendarWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  TodoCalendarWidgetBundle.swift
+//  TodoCalendarWidget
+//
+//  Created by sudo.park on 5/18/24.
+//  Copyright © 2024 com.sudo.park. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TodoCalendarWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TodoCalendarWidget()
+    }
+}
