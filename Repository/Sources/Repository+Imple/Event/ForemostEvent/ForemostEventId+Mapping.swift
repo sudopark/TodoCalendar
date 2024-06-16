9e79f68eef37e17e5aa0917b44c6ecb2c160feb9
sudo.park <sudo.park@kakao.com>
2024-06-16 14:19:25 +0900

    [#223] ForemostEventRemoteRepositoryImple 구현

--- Repository/Sources/Repository+Imple/Event/ForemostEvent/ForemostEventId+Mapping.swift
+++ Repository/Sources/Repository+Imple/Event/ForemostEvent/ForemostEventId+Mapping.swift
@@ -9,12 +9,13 @@
 import Foundation
 import Domain
 
+private enum CodingKeys: String, CodingKey {
+    case eventId = "event_id"
+    case isTodo = "is_todo"
+    case event
+}
+
 struct ForemostEventIdMapper: Codable {
-    
-    private enum CodingKeys: String, CodingKey {
-        case eventId = "event_id"
-        case isTodo = "is_todo"
-    }
     
     let id: ForemostEventId
     init(id: ForemostEventId) {
@@ -35,5 +36,47 @@ struct ForemostEventIdMapper: Codable {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(self.id.eventId, forKey: .eventId)
         try container.encode(self.id.isTodo, forKey: .isTodo)
+    }
+}
+
+extension ForemostEventId {
+    
+    func asJson() -> [String: Any] {
+        typealias Keys = CodingKeys
+        return [
+            Keys.eventId.rawValue: self.eventId,
+            Keys.isTodo.rawValue: self.isTodo
+        ]
+    }
+}
+
+struct ForemostEventIdRemoveResponseMapper: Decodable {
+    let status: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case status
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.status = try container.decode(String.self, forKey: .status)
+    }
+}
+
+
+struct ForemostMarkableEventResponseMapper: Decodable {
+    
+    let event: (any ForemostMarkableEvent)?
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let isTodo: Bool = try container.decode(Bool.self, forKey: .isTodo)
+        if isTodo {
+            let todo = try container.decodeIfPresent(TodoEventMapper.self, forKey: .event)
+            self.event = todo?.todo
+        } else {
+            let schedule = try container.decodeIfPresent(ScheduleEventMapper.self, forKey: .event)
+            self.event = schedule?.event
+        }
     }
 }
