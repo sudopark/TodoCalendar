83f6b6f41f8cb621d0c8d87b4faf7f03f26168ed
sudo.park <sudo.park@kakao.com>
2024-07-03 03:26:56 +0900

    [#212] DoubleMonthWidget 추가

---
+++ TodoCalendarApp/AppExtensions/Widget/Sources/Widgets/ComposedWidget/DoubleMonthWidget/DoubleMonthWidgetTimlineProvider.swift
@@ -0,0 +1,100 @@
+//
+//  DoubleMonthWidgetTimlineProvider.swift
+//  TodoCalendarApp
+//
+//  Created by sudo.park on 7/3/24.
+//  Copyright © 2024 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+import WidgetKit
+import Prelude
+import Optics
+import Domain
+import Extensions
+import CalendarScenes
+
+
+// MARK: - widgetViewModel
+
+struct DoubleMonthWidgetViewModel {
+    
+    let current: MonthWidgetViewModel
+    let next: MonthWidgetViewModel
+}
+
+struct DoubleMonthWidgetViewModelProvider {
+    
+    private let settingRepository: any CalendarSettingRepository
+    private let monthViewModelProvider: MonthWidgetViewModelProvider
+    
+    init(
+        settingRepository: any CalendarSettingRepository,
+        monthViewModelProvider: MonthWidgetViewModelProvider
+    ) {
+        self.settingRepository = settingRepository
+        self.monthViewModelProvider = monthViewModelProvider
+    }
+    
+    func getviewModel(_ now: Date) async throws -> DoubleMonthWidgetViewModel {
+        
+        let currentMonthModel = try await self.monthViewModelProvider.getMonthViewModel(now)
+        
+        let timeZone = self.settingRepository.loadUserSelectedTImeZone() ?? .current
+        let calendar = Calendar(identifier: .gregorian) |> \.timeZone .~ timeZone
+        let nextMonth = try calendar.addMonth(1, from: now).unwrap()
+        
+        let nextMonthModel = try await self.monthViewModelProvider.getMonthViewModel(nextMonth)
+        |> \.todayIdentifier .~ nil
+        
+        return .init(current: currentMonthModel, next: nextMonthModel)
+    }
+}
+
+struct DoubleMonthWidgetTimlineProvider: TimelineProvider {
+    
+    typealias Entry = ResultTimelineEntry<DoubleMonthWidgetViewModel>
+    
+    func placeholder(in context: Context) -> Entry {
+        return .init(date: Date()) {
+            .init(
+                current: try MonthWidgetViewModel.makeSample(),
+                next: try MonthWidgetViewModel.makeSampleNextMonth()
+            )
+        }
+    }
+    
+    func getSnapshot(in context: Context, completion: @escaping (Entry) -> Void) {
+        guard context.isPreview == false
+        else {
+            completion(placeholder(in: context))
+            return
+        }
+        self.getEntry { entry in
+            completion(entry)
+        }
+    }
+    
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+        
+        self.getEntry { entry in
+            let timeline = Timeline(entries: [entry], policy: .after(Date().nextUpdateTime))
+            completion(timeline)
+        }
+    }
+    
+    private func getEntry(_ completion: @escaping (Entry) -> Void) {
+     
+        Task {
+            let builer = WidgetViewModelProviderBuilder(base: .init())
+            let viewModelProvider = await builer.makeDoubleMonthViewModelProvider()
+            let now = Date()
+            do {
+                let model = try await viewModelProvider.getviewModel(now)
+                completion(.init(date: now, result: .success(model)))
+            } catch {
+                completion(.init(date: now, result: .failure(.init(error: error))))
+            }
+        }
+    }
+}
