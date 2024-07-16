f7954206548634c78155334ed99bf0b22d937dff
sudo.park <sudo.park@kakao.com>
2024-07-17 02:05:46 +0900

    [#212] ForemostEventWidgetViewModelProvider 추가
    - foremost evnet를 조회하여 EventCellViewModel 형식으로 변환해서, 기본 태그 색상 설정값과 함께 반환
    - 해당 과정에서 foremost event가 schedule event이고, 오늘 기준 이미 지난 이벤트의 경우에는 없는것으로 간주

---
+++ TodoCalendarApp/AppExtensions/Widget/Sources/Widgets/ForemostWidget/ForemostEventWidgetViewModel+Provider.swift
@@ -0,0 +1,96 @@
+//
+//  ForemostEventWidgetViewModel+Provider.swift
+//  TodoCalendarApp
+//
+//  Created by sudo.park on 7/17/24.
+//  Copyright © 2024 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+import Prelude
+import Optics
+import Domain
+import Extensions
+import CalendarScenes
+
+
+// MARK: - ForemostEventWidgetViewModel
+
+struct ForemostEventWidgetViewModel {
+    
+    let eventModel: (any EventCellViewModel)?
+    let defaultTagColorSetting: DefaultEventTagColorSetting
+    
+    static func sample() -> ForemostEventWidgetViewModel {
+        
+        let event = TodoEventCellViewModel("tood", name: "⛱️ Take an umbrella!")
+            |> \.tagColor .~ .default
+            |> \.periodText .~ .singleText(.init(text: "13:00"))
+        let defaultTagColorSetting = DefaultEventTagColorSetting(
+            holiday: "#D6236A", default: "#088CDA"
+        )
+        return .init(eventModel: event, defaultTagColorSetting: defaultTagColorSetting)
+    }
+}
+
+
+// MARK: ForemostEventWidgetViewModelProvider
+
+final class ForemostEventWidgetViewModelProvider {
+    
+    private let eventFetchUsecase: any CalendarEventFetchUsecase
+    private let calendarSettingRepository: any CalendarSettingRepository
+    private let appSettingRepository: any AppSettingRepository
+    
+    init(
+        eventFetchUsecase: any CalendarEventFetchUsecase,
+        calendarSettingRepository: any CalendarSettingRepository,
+        appSettingRepository: any AppSettingRepository
+    ) {
+        self.eventFetchUsecase = eventFetchUsecase
+        self.calendarSettingRepository = calendarSettingRepository
+        self.appSettingRepository = appSettingRepository
+    }
+}
+
+extension ForemostEventWidgetViewModelProvider {
+    
+    func getViewModel(_ refTime: Date) async throws -> ForemostEventWidgetViewModel {
+        
+        let setting = self.appSettingRepository.loadSavedViewAppearance()
+        let eventModel = try await self.loadForemostEventModel(refTime, setting.calendar)
+        return ForemostEventWidgetViewModel(
+            eventModel: eventModel,
+            defaultTagColorSetting: setting.defaultTagColor
+        )
+    }
+    
+    private func loadForemostEventModel(
+        _ refTime: Date,
+        _ setting: CalendarAppearanceSettings
+    ) async throws -> (any EventCellViewModel)? {
+        let timeZone = self.calendarSettingRepository.loadUserSelectedTImeZone() ?? .current
+        let calendar = Calendar(identifier: .gregorian) |> \.timeZone .~ timeZone
+        let dayRange = try calendar.dayRange(refTime).unwrap()
+        let eventAndTag = try await self.eventFetchUsecase.fetchForemostEvent()
+        var model: (any EventCellViewModel)? = {
+            switch eventAndTag.foremostEvent {
+            case let todo as TodoEvent:
+                let event = TodoCalendarEvent(todo, in: timeZone, isForemost: true)
+                return TodoEventCellViewModel(
+                    event, in: dayRange, timeZone, setting.is24hourForm
+                )
+            case let schedule as ScheduleEvent:
+                let isPast = schedule.time.lowerBoundWithFixed < dayRange.lowerBound
+                guard !isPast else { return nil }
+                let event = ScheduleCalendarEvent.events(from: schedule, in: timeZone)[0]
+                return ScheduleEventCellViewModel(
+                    event, in: dayRange, timeZone: timeZone, setting.is24hourForm
+                )
+            default: return nil
+            }
+        }()
+        model?.applyTagColor(eventAndTag.tag)
+        return model
+    }
+}
---
+++ TodoCalendarApp/AppExtensions/Widget/Tests/ViewModelProviders/ForemostEventWidgetViewModelProviderTests.swift
@@ -0,0 +1,143 @@
+//
+//  ForemostEventWidgetViewModelProviderTests.swift
+//  TodoCalendarAppWidgetTests
+//
+//  Created by sudo.park on 7/17/24.
+//  Copyright © 2024 com.sudo.park. All rights reserved.
+//
+
+import XCTest
+import Prelude
+import Optics
+import Domain
+import Extensions
+import CalendarScenes
+import UnitTestHelpKit
+import TestDoubles
+
+
+class ForemostEventWidgetViewModelProviderTests: XCTestCase {
+    
+    private func makeProvider(
+        _ foremostEvent: (any ForemostMarkableEvent)?,
+        shouldFail: Bool = false
+    ) -> ForemostEventWidgetViewModelProvider {
+        
+        let usecase = PrivateStubEventFetchUsecase()
+        usecase.shouldFailFetchForemost = shouldFail
+        usecase.foremostEvent = foremostEvent
+        
+        let calendarSettingRepository = StubCalendarSettingRepository()
+        let appSettingRepository = StubAppSettingRepository()
+        
+        return .init(
+            eventFetchUsecase: usecase,
+            calendarSettingRepository: calendarSettingRepository,
+            appSettingRepository: appSettingRepository
+        )
+    }
+    
+    private var kst: TimeZone { TimeZone(abbreviation: "KST")! }
+    
+    private var refTime: Date {
+        let calenadr = Calendar(identifier: .gregorian) |> \.timeZone .~ self.kst
+        return calenadr.dateBySetting(from: Date()) {
+            $0.year = 2024; $0.month = 3; $0.day = 1
+        }!
+    }
+    
+    private var dummyTodo: TodoEvent {
+        return TodoEvent(uuid: "todo", name: "todo")
+    }
+    
+    private var dummySchedule: ScheduleEvent {
+        return ScheduleEvent(uuid: "schedule", name: "schedule", time: .at(refTime.timeIntervalSince1970 + 10))
+    }
+    
+    private var dummPastSchedule: ScheduleEvent {
+        let past = self.refTime.add(days: -1)!
+        return ScheduleEvent(uuid: "past-schedule", name: "past-schedule", time: .at(past.timeIntervalSince1970))
+    }
+}
+
+
+extension ForemostEventWidgetViewModelProviderTests {
+    
+    // foremost event: todo인 경우
+    func testProvider_provideForemostEvent_isTodo() async throws {
+        // given
+        let provider = self.makeProvider(self.dummyTodo)
+        
+        // when
+        let model = try await provider.getViewModel(self.refTime)
+        
+        // then
+        XCTAssertEqual(model.eventModel?.eventIdentifier, "todo")
+    }
+    
+    // foremost event: schedule event 인 경우
+    func testProvider_provideForemostEvent_isScheduleEvent() async throws {
+        // given
+        let provider = self.makeProvider(self.dummySchedule)
+        
+        // when
+        let model = try await provider.getViewModel(self.refTime)
+        
+        // then
+        XCTAssertEqual(model.eventModel?.eventIdentifier, "schedule-1")
+    }
+    
+    // foremost event: 이미 지난 schedule event 인 경우 -> 없는것으로 취급
+    func testProvider_whenForemostEventIsPastScheduleEvent_regardAsNotExists() async throws {
+        // given
+        let provider = self.makeProvider(self.dummPastSchedule)
+        
+        // when
+        let model = try await provider.getViewModel(self.refTime)
+        
+        // then
+        XCTAssertNil(model.eventModel)
+    }
+    
+    // 없는경우 결과 nil
+    func testProvider_provideFremostEvnet_notExists() async throws {
+        // given
+        let provider = self.makeProvider(nil)
+        
+        // when
+        let model = try await provider.getViewModel(self.refTime)
+        
+        // then
+        XCTAssertNil(model.eventModel)
+    }
+    
+    // 조회 실패시 에러
+    func testProvider_whenProvideFailed_error() async {
+        // given
+        let provider = self.makeProvider(nil, shouldFail: true)
+        var failed: (any Error)?
+        // when
+        do {
+            _ = try await provider.getViewModel(self.refTime)
+        } catch {
+            failed = error
+        }
+        
+        // then
+        XCTAssertNotNil(failed)
+    }
+}
+
+private final class PrivateStubEventFetchUsecase: StubCalendarEventsFetchUescase {
+    
+    var shouldFailFetchForemost: Bool = false
+    var foremostEvent: (any ForemostMarkableEvent)?
+    override func fetchForemostEvent() async throws -> ForemostEventAndTag {
+        guard self.shouldFailFetchForemost == false
+        else {
+            throw RuntimeError("failed")
+        }
+        
+        return .init(foremostEvent: foremostEvent, tag: nil)
+    }
+}
