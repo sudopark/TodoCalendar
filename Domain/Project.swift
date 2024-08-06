a00c03c22797290b048a19f7a6c50bcc939bc0f1
sudo.park <sudo.park@kakao.com>
2024-08-06 14:56:15 +0900

    [#250] Domain 및 repository 최소 os 버전 ios 17로 상향
    - 테스트 타켓에서 -> testdouble 프레임워크(ios 17) 의존하고있음

--- Domain/Project.swift
+++ Domain/Project.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 
 let project = Project.frameworkWithTest(name: "Domain",
                                         platform: .iOS,
-                                        iOSTargetVersion: "16.0",
+                                        iOSTargetVersion: "17.0",
                                         dependencies: [
                                             .project(target: "Common3rdParty", path: .relativeToCurrentFile("../Supports/Common3rdParty")),
                                             .project(target: "Extensions", path: .relativeToCurrentFile("../Supports/Extensions"))
