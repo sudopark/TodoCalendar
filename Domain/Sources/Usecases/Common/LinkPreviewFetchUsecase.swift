f5d87c248a0024ee32d8f8165fe44766163c3262
sudo.park <sudo.park@kakao.com>
2024-08-10 14:14:26 +0900

    [#254] LinkPreviewFetchUsecaesImple 추가

---
+++ Domain/Sources/Usecases/Common/LinkPreviewFetchUsecase.swift
@@ -0,0 +1,70 @@
+//
+//  LinkPreviewFetchUsecase.swift
+//  Domain
+//
+//  Created by sudo.park on 8/10/24.
+//  Copyright © 2024 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+import SwiftLinkPreview
+
+
+public protocol LinkPreviewFetchUsecase: AnyObject, Sendable {
+    
+    func fetchPreview(_ url: URL) async throws -> LinkPreview
+}
+
+
+public final class LinkPreviewFetchUsecaesImple: LinkPreviewFetchUsecase, @unchecked Sendable {
+    
+    private let previewEngine: SwiftLinkPreview
+    public init(previewEngine: SwiftLinkPreview) {
+        self.previewEngine = previewEngine
+    }
+}
+
+extension LinkPreviewFetchUsecaesImple {
+    
+    final class FetchTask {
+        let engine: SwiftLinkPreview
+        let url: URL
+        private var request: Cancellable?
+        
+        init(_ engine: SwiftLinkPreview, _ url: URL) {
+            self.engine = engine
+            self.url = url
+        }
+        
+        func fetch() async throws -> LinkPreview {
+            let path = self.url.absoluteString
+            return try await withCheckedThrowingContinuation { [weak self] continuation in
+                self?.request = self?.engine.preview(path) { response in
+                    let preview = LinkPreview(
+                        url: response.url,
+                        title: response.title,
+                        description: response.description,
+                        mainImagePath: response.image,
+                        images: response.images ?? []
+                    )
+                    continuation.resume(returning: preview)
+                } onError: { error in
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+        
+        func cancel() {
+            self.request?.cancel()
+        }
+    }
+    
+    public func fetchPreview(_ url: URL) async throws -> LinkPreview {
+        let fetchTask = FetchTask(self.previewEngine, url)
+        return try await withTaskCancellationHandler {
+            return try await fetchTask.fetch()
+        } onCancel: {
+            fetchTask.cancel()
+        }
+    }
+}
---
+++ Supports/TestDoubles/Sources/Usecases/StubLinkPreviewFetchUsecase.swift
@@ -0,0 +1,23 @@
+//
+//  StubLinkPreviewFetchUsecase.swift
+//  TestDoubles
+//
+//  Created by sudo.park on 8/10/24.
+//  Copyright © 2024 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+import Domain
+
+
+open class StubLinkPreviewFetchUsecase: LinkPreviewFetchUsecase, @unchecked Sendable {
+    
+    public init() { }
+    
+    open func fetchPreview(_ url: URL) async throws -> LinkPreview {
+        let preview = LinkPreview(
+            url: url, title: "title", description: "description", mainImagePath: "image", images: []
+        )
+        return preview
+    }
+}
