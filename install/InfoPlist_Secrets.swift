58f12c248e202fab9902476ee1e212bd25151b36
sudo.park <sudo.park@kakao.com>
2024-09-21 12:22:51 +0900

    [#292] project install + build을 위한 파일 추가

---
+++ install/InfoPlist_Secrets.swift
@@ -0,0 +1,15 @@
+//
+//  InfoPlist_Secrets.swift
+//  TodoCalendarAppManifests
+//
+//  Created by sudo.park on 2023/12/16.
+//
+
+import ProjectDescription
+
+
+extension Project {
+    
+    static let googleClientId: String = "dummy.id"
+    static let googleReverseAppId: String = "id.dummy"
+}
