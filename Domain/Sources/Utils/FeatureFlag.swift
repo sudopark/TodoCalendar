1c5ee5e9140946df2f1b3c8b3a8b4cefb0a04bdb
sudo.park <sudo.park@kakao.com>
2025-02-28 00:27:00 +0900

    #354 FeatureFlag 추가

---
+++ Domain/Sources/Utils/FeatureFlag.swift
@@ -0,0 +1,34 @@
+//
+//  FeatureFlag.swift
+//  Domain
+//
+//  Created by sudo.park on 2/28/25.
+//  Copyright © 2025 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+
+
+public final class FeatureFlag: @unchecked Sendable {
+    
+    public enum Flags: Sendable {
+        case googleCalendar
+    }
+    
+    private var enableFlags: Set<Flags> = []
+    
+    private static let shared: FeatureFlag = .init()
+    private init() { }
+}
+
+
+extension FeatureFlag {
+    
+    public static func enable(_ flag: Flags) {
+        FeatureFlag.shared.enableFlags.insert(flag)
+    }
+    
+    public static func isEnable(_ flag: Flags) -> Bool {
+        return FeatureFlag.shared.enableFlags.contains(flag)
+    }
+}
