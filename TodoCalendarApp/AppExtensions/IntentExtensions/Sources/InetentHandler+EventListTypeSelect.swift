c59b74bee3e4e95f8d2591548dbbc88b66828fa8
sudo.park <sudo.park@kakao.com>
2025-06-08 12:17:46 +0900

    [#382] InetentHandler+EventListTypeSelect 구글캘린더 목록 제공 추가

--- TodoCalendarApp/AppExtensions/IntentExtensions/Sources/InetentHandler+EventListTypeSelect.swift
+++ TodoCalendarApp/AppExtensions/IntentExtensions/Sources/InetentHandler+EventListTypeSelect.swift
@@ -18,16 +18,38 @@ extension IntentHandler: EventListTypeSelectIntentHandling {
     ) async throws -> INObjectCollection<EvnetListType> {
         
         let factory = IntentReposiotryFactory(base: AppExtensionBase())
-        let repository = factory.makeEventTagRepository()
-        var allTags: [CustomEventTag] = []
-        for try await ts in repository.loadAllCustomTags().values {
-            allTags = ts
+        let tagRepository = factory.makeEventTagRepository()
+        let externalCalendarRepository = factory.makeExternalCalendarAcountRepository()
+        let googleRepository = factory.makeGoogleCalendarRepository()
+        
+        var allTags: [Tags] = []
+        for try await ts in tagRepository.loadAllCustomTags().values {
+            allTags = ts.map { .custom($0) }
         }
+        let googleServiceId = GoogleCalendarService.id
+        let externalAccount = try await externalCalendarRepository.loadIntegratedAccounts().asDictionary { $0.serviceIdentifier }
+        if externalAccount[googleServiceId] != nil {
+            let externalTags = try await googleRepository.loadCalendarTags().values
+                .first(where: { _ in true })?
+                .filter { !$0.isHoliday }
+                .map { Tags.external(googleServiceId, $0) } ?? []
+            allTags += externalTags
+        }
+        
         let defaultType = EvnetListType(identifier: "default", display: "All")
         let types = allTags.map {
-            EvnetListType(identifier: $0.uuid, display: $0.name)
+            switch $0 {
+            case .custom(let tag):
+                return EvnetListType(identifier: tag.uuid, display: tag.name)
+            case .external(let serviceId, let tag):
+                return EvnetListType(identifier: "external::\(serviceId)::\(tag.id)", display: tag.name)
+            }
         }
         return INObjectCollection(items: [defaultType] + types)
     }
 }
 
+private enum Tags {
+    case custom(CustomEventTag)
+    case external(String, GoogleCalendar.Tag)
+}
