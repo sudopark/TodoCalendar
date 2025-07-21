85807107b03b753e79b05d3890fc53d5cd6c9a65
sudo.park <sudo.park@kakao.com>
2025-07-22 00:59:18 +0900

    [#414] EventUploadPendingQueueStorage 구현
    - 업로드 할 작업 EventUploadPendingQueueTable에 push
    - pop시 timestamp 오름차순으로 저장된 데이터 중 첫번째 원소를 읽어와서, 이를 삭제

---
+++ Repository/Sources/Repository+Imple/Event/Upload/Local/EventUploadPendingQueueLocalStorage.swift
@@ -0,0 +1,52 @@
+//
+//  EventUploadPendingQueueLocalStorage.swift
+//  Repository
+//
+//  Created by sudo.park on 7/22/25.
+//  Copyright © 2025 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+@preconcurrency import SQLiteService
+import Domain
+import Extensions
+
+
+public protocol EventUploadPendingQueueLocalStorage: Sendable {
+    
+    func popTask() async throws -> EventUploadingTask?
+    func pushTask(_ task: EventUploadingTask) async throws
+}
+
+
+public final class EventUploadPendingQueueLocalStorageImple: EventUploadPendingQueueLocalStorage {
+    
+    private let sqliteService: SQLiteService
+    public init(sqliteService: SQLiteService) {
+        self.sqliteService = sqliteService
+    }
+    
+    private typealias Queue = EventUploadPendingQueueTable
+}
+
+extension EventUploadPendingQueueLocalStorageImple {
+    
+    public func popTask() async throws -> EventUploadingTask? {
+        try await self.sqliteService.async.run { db in
+            let query = Queue.selectAll()
+                .orderBy(isAscending: true) { $0.timestamp }
+            guard let firstTask = try db.loadOne(Queue.self, query: query)
+            else { return nil }
+            
+            let deleteQuery = Queue.delete().where { $0.uuid == firstTask.uuid }
+            try db.delete(Queue.self, query: deleteQuery)
+            return firstTask
+        }
+    }
+    
+    public func pushTask(_ task: EventUploadingTask) async throws {
+        try await self.sqliteService.async.run { db in
+            try db.insertOne(Queue.self, entity: task, shouldReplace: true)
+        }
+    }
+}
