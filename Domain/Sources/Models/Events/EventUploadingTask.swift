73f30c9ae3e457a1c9da9548953b5bc0bf8a817b
sudo.park <sudo.park@kakao.com>
2025-07-22 00:59:18 +0900

    [#414] EventUploadPendingQueueStorage 구현
    - 업로드 할 작업 EventUploadPendingQueueTable에 push
    - pop시 timestamp 오름차순으로 저장된 데이터 중 첫번째 원소를 읽어와서, 이를 삭제

---
+++ Domain/Sources/Models/Events/EventUploadingTask.swift
@@ -0,0 +1,36 @@
+//
+//  EventUploadingTask.swift
+//  Domain
+//
+//  Created by sudo.park on 7/22/25.
+//  Copyright © 2025 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+
+
+public struct EventUploadingTask: Sendable {
+    
+    public enum DataType: String, Sendable {
+        case eventTag
+        case todo
+        case schedule
+    }
+    
+    public var timestamp: TimeInterval
+    public let dataType: DataType
+    public let uuid: String
+    public let isRemovingTask: Bool
+    
+    public init(
+        timestamp: TimeInterval = Date().timeIntervalSince1970,
+        dataType: DataType,
+        uuid: String,
+        isRemovingTask: Bool
+    ) {
+        self.timestamp = timestamp
+        self.dataType = dataType
+        self.uuid = uuid
+        self.isRemovingTask = isRemovingTask
+    }
+}
---
+++ Repository/Sources/Repository+Imple/Event/Upload/Local/EventUploadPendingQueueTable.swift
@@ -0,0 +1,59 @@
+//
+//  EventUploadPendingQueueTable.swift
+//  Repository
+//
+//  Created by sudo.park on 7/21/25.
+//  Copyright © 2025 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+import SQLiteService
+import Domain
+import Extensions
+
+
+struct EventUploadPendingQueueTable: Table {
+    
+    enum Colunms: String, TableColumn {
+        case timestamp
+        case dataType = "data_type"
+        case uuid
+        case isRemove = "is_remove"
+        
+        var dataType: ColumnDataType {
+            switch self {
+            case .timestamp: return .real([.notNull])
+            case .dataType: return .text([.notNull])
+            case .uuid: return .text([.unique, .notNull])
+            case .isRemove: return .integer([.default(0), .notNull])
+            }
+        }
+    }
+    
+    typealias ColumnType = Colunms
+    typealias EntityType = EventUploadingTask
+    static var tableName: String { "event_upload_pending_queue" }
+    
+    static func scalar(_ entity: EntityType, for column: Colunms) -> (any ScalarType)? {
+        switch column {
+        case .timestamp: return entity.timestamp
+        case .dataType: return entity.dataType.rawValue
+        case .uuid: return entity.uuid
+        case .isRemove: return entity.isRemovingTask
+        }
+    }
+}
+
+extension EventUploadingTask: @retroactive RowValueType {
+    
+    public init(_ cursor: CursorIterator) throws {
+        
+        self.init(
+            timestamp: try cursor.next().unwrap(),
+            dataType: try EventUploadingTask.DataType(
+                rawValue: try cursor.next().unwrap()).unwrap(),
+            uuid: try cursor.next().unwrap(),
+            isRemovingTask: try cursor.next().unwrap()
+        )
+    }
+}
