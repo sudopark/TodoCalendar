b632c9ddf8d2dbfa22bb0855d1e0ab1694107301
sudo.park <sudo.park@kakao.com>
2025-10-21 00:55:48 +0900

    [#386] DaysIntervalCountUsecase 구현
    - 주어진 eventTime, holiday 기준 d-day 계산
    - 날짜변경시, timeZone 변경시 카운팅

---
+++ Domain/Sources/Usecases/Events/DaysIntervalCountUsecase.swift
@@ -0,0 +1,77 @@
+//
+//  DaysIntervalCountUsecase.swift
+//  Domain
+//
+//  Created by sudo.park on 10/20/25.
+//  Copyright © 2025 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+import Combine
+import Prelude
+import Optics
+
+
+public protocol DaysIntervalCountUsecase: Sendable {
+    
+    func countDays(to eventTime: EventTime) -> AnyPublisher<Int, Never>
+    func countDays(to holiday: Holiday) -> AnyPublisher<Int, Never>
+}
+
+
+public final class DaysIntervalCountUsecaseImple: DaysIntervalCountUsecase, @unchecked Sendable {
+    
+    private let calendarSettingUsecase: any CalendarSettingUsecase
+    public init(calendarSettingUsecase: any CalendarSettingUsecase) {
+        self.calendarSettingUsecase = calendarSettingUsecase
+    }
+}
+
+
+extension DaysIntervalCountUsecaseImple {
+    
+    public func countDays(
+        to eventTime: EventTime
+    ) -> AnyPublisher<Int, Never> {
+        return self.countInterval { timeZone in
+            return Date(
+                timeIntervalSince1970: eventTime.rangeWithShifttingifNeed(on: timeZone).lowerBound
+            )
+        }
+    }
+    
+    public func countDays(
+        to holiday: Holiday
+    ) -> AnyPublisher<Int, Never> {
+        return self.countInterval { timeZone in
+            return holiday.date(at: timeZone)
+        }
+    }
+    
+    private func countInterval(
+        _ selectToDate: @escaping (TimeZone) -> Date?
+    ) -> AnyPublisher<Int, Never> {
+        
+        let transform: (Date, TimeZone) -> Int? = { fromDate, timeZone in
+            guard let toDate = selectToDate(timeZone) else { return nil }
+            let calendar = Calendar(identifier: .gregorian) |> \.timeZone .~ timeZone
+            return calendar.diffDays(fromDate, toDate)
+        }
+     
+        return Publishers.CombineLatest(
+            self.secondTicks,
+            self.calendarSettingUsecase.currentTimeZone
+        )
+        .compactMap(transform)
+        .removeDuplicates()
+        .eraseToAnyPublisher()
+    }
+    
+    private var secondTicks: AnyPublisher<Date, Never> {
+        return Timer
+            .publish(every: 1.0, on: .main, in: .common).autoconnect()
+            .map { _ in Date() }
+            .prepend(Date())
+            .eraseToAnyPublisher()
+    }
+}
