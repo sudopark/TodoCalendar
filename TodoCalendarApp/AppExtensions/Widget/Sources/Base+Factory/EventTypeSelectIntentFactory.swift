24263d2c7e3ad44244a171041417d58ed0c093ad
sudo.park <sudo.park@kakao.com>
2025-10-26 22:41:02 +0900

    [#465] EventListWidget에서 노출할 복수의 이벤트 종류 선택이 가능한 EventTypeSelectIntent 구현
    - 선택가능 이벤트 타입: EventTypeEntity
    - 선택가능 이벤트 타입 조회, 선택된 이벤트. 타입 조회: EventTypeQuery
    - intent: EventTypeSelectIntent
    ㄴ eventTypes: 기본값 nil => nil인 경우 필터링 안함(예정)

---
+++ TodoCalendarApp/AppExtensions/Widget/Sources/Base+Factory/EventTypeSelectIntentFactory.swift
@@ -0,0 +1,70 @@
+//
+//  EventTypeSelectIntentFactory.swift
+//  TodoCalendarApp
+//
+//  Created by sudo.park on 10/26/25.
+//  Copyright © 2025 com.sudo.park. All rights reserved.
+//
+
+import Foundation
+import Prelude
+import Optics
+import Domain
+import Extensions
+import Repository
+
+
+struct EventTypeSelectIntentFactory {
+    
+    private let base: AppExtensionBase
+    init(base: AppExtensionBase) {
+        self.base = base
+    }
+}
+
+extension EventTypeSelectIntentFactory {
+    
+    func makeEventTagRepository() -> any EventTagRepository {
+        
+        let auth = self.base.authStore.loadCurrentAuth()
+        let localStorage = EventTagLocalStorageImple(sqliteService: base.commonSqliteService)
+        let todoLocalStorage = TodoLocalStorageImple(sqliteService: base.commonSqliteService)
+        let scheduleLocalStorage = ScheduleEventLocalStorageImple(sqliteService: base.commonSqliteService)
+        
+        if let auth {
+            let remote = base.remoteAPI
+            let credential = APICredential(auth: auth)
+            remote.setup(credential: credential)
+            return EventTagRemoteRepositoryImple(
+                remote: EventTagRemoteImple(remote: remote),
+                cacheStorage: localStorage,
+                todoCacheStorage: todoLocalStorage,
+                scheduleCacheStorage: scheduleLocalStorage,
+                environmentStorage: base.userDefaultEnvironmentStorage
+            )
+        } else {
+            return EventTagLocalRepositoryImple(
+                localStorage: localStorage,
+                todoLocalStorage: todoLocalStorage,
+                scheduleLocalStorage: scheduleLocalStorage,
+                environmentStorage: base.userDefaultEnvironmentStorage
+            )
+        }
+    }
+    
+    func makeExternalCalendarAcountRepository() -> any ExternalCalendarIntegrateRepository {
+        
+        return ExternalCalendarIntegrateRepositoryImple(
+            supportServices: AppEnvironment.supportExternalCalendarServices,
+            removeAPIPerService: [:],
+            keyChainStore: self.base.keyChainStorage
+        )
+    }
+    
+    func makeGoogleCalendarRepository() -> any GoogleCalendarRepository {
+        return GoogleCalendarRepositoryImple(
+            remote: EmptyRemote(),
+            cacheStorage: GoogleCalendarLocalStorageImple(sqliteService: base.commonSqliteService)
+        )
+    }
+}
