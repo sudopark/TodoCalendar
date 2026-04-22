52e9c0a628e3b994bc8f41c9cd100d6cde9c1b4e
sudo.park <sudo.park@kakao.com>
2026-04-23 00:00:20 +0900

    v2.9.2

--- Tuist/ProjectDescriptionHelpers/Project+AppVersion.swift
+++ Tuist/ProjectDescriptionHelpers/Project+AppVersion.swift
@@ -10,6 +10,6 @@ import ProjectDescription
 
 extension Project {
     
-    public static let appVersion = "2.9.1"
+    public static let appVersion = "2.9.2"
     public static let buildNumber = "1"
 }
