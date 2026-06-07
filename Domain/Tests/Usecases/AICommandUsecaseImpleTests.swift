70d857b33e84dc6f235513243c5fdfc9f49ea5b5
sudo.park <sudo.park@kakao.com>
2026-06-07 13:06:35 +0900

    [#635] AIAgentUsageUsecase 구현
    - 사용량 조회 및 전파

--- Domain/Tests/Usecases/AICommandUsecaseImpleTests.swift
+++ Domain/Tests/Usecases/AICommandUsecaseImpleTests.swift
@@ -21,7 +21,7 @@ import TestDoubles
 final class AICommandUsecaseImpleTests: PublisherWaitable {
     
     var cancelBag: Set<AnyCancellable>! = []
-    private var stubRepository = PrivateStubRepository()
+    private var stubRepository = BaseStubAICommandRepository()
     
     private func makeUsecase(
         shouldFailMakeJob: Bool = false,
@@ -557,66 +557,5 @@ private extension ServerErrorModel {
     static func dummy(_ code: ServerErrorModel.ErrorCode) -> ServerErrorModel {
         return .init()
             |> \.code .~ code
-    }
-}
-
-private final class PrivateStubRepository: AICommandRepository, @unchecked Sendable {
-    
-    var shouldFailProcessCommand: Bool = false
-    func processCommand(_ commandText: String, timeZone: String) async throws -> String {
-        guard !self.shouldFailProcessCommand
-        else {
-            throw RuntimeError("not imple")
-        }
-        return "some_job"
-    }
-
-    var shouldFailProcessConfirmCommand: Bool = false
-    func processConfirmCommand(_ action: AIConfirmCommandAction, timeZone: String) async throws -> String {
-        guard !self.shouldFailProcessConfirmCommand
-        else {
-            throw RuntimeError("not imple")
-        }
-        return "some_job"
-    }
-    
-    
-    var stubLoadJobs: [Result<AIJob, any Error>] = []
-    var loadJobMocking: Result<AIJob, any Error>?
-    func loadJob(_ jobId: String) async throws -> AIJob {
-        
-        if let mocking = self.loadJobMocking {
-            switch mocking {
-            case .success(let job): return job
-            case .failure(let error): throw error
-            }
-        }
-        
-        if self.stubLoadJobs.isEmpty {
-            throw RuntimeError("failed")
-        }
-        
-        let first = self.stubLoadJobs.removeFirst()
-        switch first {
-        case .success(let job): return job
-        case .failure(let error): throw error
-        }
-    }
-    
-    private var processingCmd: ProcessingAICommand?
-    func updateProcessingAICommand(_ cmd: ProcessingAICommand) async throws {
-        self.processingCmd = cmd
-    }
-    
-    func loadProcessingAICommand() async throws -> ProcessingAICommand? {
-        return self.processingCmd
-    }
-    
-    func clearProcessingAICommand() async throws {
-        self.processingCmd = nil
-    }
-    
-    func loadUsage() async throws -> AIAgentUsage {
-        throw RuntimeError("not imple")
     }
 }
